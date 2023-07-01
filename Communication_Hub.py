0354667548a4efd0abeebf3901d799027eb666e1
Jollyjep <Dcole875@gmail.com>
2023-07-01 02:04:54 +0100

    Started work on modular lighting patterns

--- Communication_Hub.py
+++ Communication_Hub.py
@@ -3,6 +3,7 @@ import threading as th
 import time
 import sacn
 import win_precise_time as wpt
+from Lighting_Modules_Air_DMX import Background as bg
 
 def Communication_Daemon(comms_queue_1, comms_queue_2, comms_queue_3, comms_queue_4, kill_switch, protocol, next_prot_queue, event):
     while len(kill_switch.qsize()) == 0:
@@ -32,6 +33,7 @@ def Air_DMX (comms_queue_2, comms_queue_4, kill_switch, framerate):
             protocol = comms_queue_2.get(blocking=False)
         except:
             protocol = "null"
+            wpt.sleep(0.2)
         if protocol != "E1.31" or protocol !="null":
             start = True
             if is_active:
@@ -50,16 +52,28 @@ def Air_DMX (comms_queue_2, comms_queue_4, kill_switch, framerate):
                 data_2 = [0, 0, 0] * 50
                 sender[1].dmx_data = data_1
                 sender[2].dmx_data = data_2
-                for x in range(120):
+                for x in range(2*framerate):
                     init_time = wpt.time()
                     data_1 = [0, 0, int(255 * (x /255)/(120/255))] * 150
                     data_2 = [0, 0, int(255 * (x /255)/(120/255))] * 50
-                    wpt.sleep(1/60 - (wpt.time()-init_time))
+                    sender[1].dmx_data = data_1
+                    sender[2].dmx_data = data_2
+                    wpt.sleep(1/framerate - (wpt.time()-init_time))
                 comms_queue_4.put(True)
 
 
-
-
+def Air_DMX_Streamer (comms_queue_4, kill_switch):
+    status = False
+    pattern = "Bg"
+    while len(kill_switch.qsize()) == 0:
+        if not status:
+            status = comms_queue_4.get()
+        else:
+            try:
+                status = comms_queue_4.get(blocking=False)
+            except:
+                status = True
+            if status:
 
 
 
---
+++ Lighting_Modules_Air_DMX/Background.py
@@ -0,0 +1,10 @@
+import win_precise_time
+import colorsys
+import numpy as np
+import colorutils
+
+class Background:
+    def __init__(self, fps):
+        self.fps = fps
+
+    def main(self):
